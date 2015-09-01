977ca1917ddc9b454e070e09fb09207618f31c2c
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-01 15:58:59 -0400

    bug fix, line 21 was failing due to type error

--- nightmare_mode_squares.py
+++ nightmare_mode_squares.py
@@ -3,10 +3,10 @@ def number():
     takes no arguements"""
     try:
         user_num = float(input("Please enter a number."))
-        if user_num < 0:
-            return (abs(user_num), "i")
+        if user_num < 0: #
+            return (abs(user_num), "i") #i is used here to represent sqrt(-1) it's not operable but it'd tell a human what's happening
         else:
-            return (user_num, 1)
+            return (user_num, 1) #the 1 here is to prevent an error when we unpack below it looks funny but it's still correct
     except:
         print('that\'s not a number at all!')
         number()
@@ -18,4 +18,4 @@ while round(y, 3) != round((y+(x/y))/2, 3):
     counter += 1
     y = (y+(x/y))/2
     print("this is attempt number " + str(counter) + " and the current guess is " + str(y))
-print("the square root is " + str(y) + "*" + z + " and it took me " + str(counter) + " attempts!")
+print("the square root is " + str(y) + "*" + str(z) + " and it took me " + str(counter) + " attempts!")
