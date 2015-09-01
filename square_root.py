43af0bcfe053dfa879b8409a84bc137aadc6864a
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-01 15:01:13 -0400

    Added code to find approximate the sqrt and a counter to track current guess and number of attempts

--- square_root.py
+++ square_root.py
@@ -9,3 +9,12 @@ def number():
     except:
         print('that\'s not a number at all!')
         number()
+user_num = number()
+y = 1
+x = user_num
+counter = 0
+while round(y, 3) != round((y+(x/y))/2, 3):
+    counter += 1
+    y = (y+(x/y))/2
+    print("this is attempt number " + str(counter) + " and the current guess is " + str(y))
+print("the square root is " + str(y) + " and it took me " + str(counter) + " attempts!")
